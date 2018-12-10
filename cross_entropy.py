770fa4bc87a84954c72a808c25ab95846ed704ab
Luan <luann.porfirio@gmail.com>
2018-12-10 08:53:28 -0200

    organizing folder

--- cross_entropy.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-# takes as input two lists Y, P 
-#and returns the float corresponding to their cross-entropy.
-def cross_entropy(Y, P):
-    Y = np.asarray(Y)
-    P = np.asarray(P)
-    return -sum(Y * np.log(P) +  (1 - Y) * np.log(1 - P))
-    
