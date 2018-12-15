cfe93eb2ed81c915d94ed7a066aa5590bb426cb7
Luan <luann.porfirio@gmail.com>
2018-12-15 12:08:49 -0200

    add validation subset

--- lesson5/mnist_mlp.py
+++ lesson5/mnist_mlp.py
@@ -1,14 +1,15 @@
 import torch
-import numpy as np
+from torch.utils.data.sampler import SubsetRandomSampler
 from torchvision import datasets
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
+import numpy as np
 
 # number of subprocesses to use for data loading
 num_workers = 0
 # how many samples per batch to load
 batch_size = 20
-
+valid_size = 0.2
 # convert data to torch.FloatTensor
 transform = transforms.ToTensor()
 
@@ -18,13 +19,26 @@ train_data = datasets.MNIST(root='.pytorch/MNIST_data/', train=True,
 test_data = datasets.MNIST(root='.pytorch/MNIST_data/', train=False,
                                           download=True, transform=transform)
 
+
+# obtain training indices that will be used for validation
+num_train = len(train_data)
+indices = list(range(num_train))
+np.random.shuffle(indices)
+split = int(np.floor(valid_size * num_train))
+train_idx, valid_idx = indices[split:], indices[:split]
+
+# define samplers for obtaining training and validation batches
+train_sampler = SubsetRandomSampler(train_idx)
+valid_sampler = SubsetRandomSampler(valid_idx)
+
 # prepare data loaders
 train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size,
-            num_workers=num_workers)
+            sampler=train_sampler, num_workers=num_workers)
+valid_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, 
+            sampler=valid_sampler, num_workers=num_workers)
 test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, 
             num_workers=num_workers)
 
-    
 # obtain one batch of training images
 dataiter = iter(train_loader)
 images, labels = dataiter.next()
@@ -38,3 +52,6 @@ for idx in np.arange(20):
     # print out the correct label for each image
     # .item() gets the value contained in a Tensor
     ax.set_title(str(labels[idx].item()))
+
+
+
