12002684bc9e0c8531803230bf0a29882fe3f653
Luan <luann.porfirio@gmail.com>
2018-12-16 11:29:12 -0200

    lesson 5: CNN Classification for CIFAR-10 databsae

---
+++ lesson5/cifar_nn.py
@@ -0,0 +1,193 @@
+from torch import nn,optim, utils
+import torch
+import torch.nn.functional as F
+from torch.utils.data.sampler import SubsetRandomSampler
+from torchvision import datasets
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+
+# number of subprocesses to use for data loading
+num_workers = 0
+# how many samples per batch to load
+batch_size = 20
+valid_size = 0.2
+
+# convert data to Normalized torch.FloatTensor
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+# choose the training and test datasets
+train_data = datasets.CIFAR10(root='.pytorch/CIFAR10_data/', train=True,
+                                           download=True, transform=transform)
+test_data = datasets.CIFAR10(root='.pytorch/CIFAR10_data/', train=False,
+                                          download=True, transform=transform)
+
+
+# obtain training indices that will be used for validation
+num_train = len(train_data)
+indices = list(range(num_train))
+np.random.shuffle(indices)
+split = int(np.floor(valid_size * num_train))
+train_idx, valid_idx = indices[split:], indices[:split]
+
+# define samplers for obtaining training and validation batches
+train_sampler = SubsetRandomSampler(train_idx)
+valid_sampler = SubsetRandomSampler(valid_idx)
+
+# prepare data loaders
+train_loader = utils.data.DataLoader(train_data, batch_size=batch_size,
+            sampler=train_sampler, num_workers=num_workers)
+valid_loader = utils.data.DataLoader(train_data, batch_size=batch_size, 
+            sampler=valid_sampler, num_workers=num_workers)
+test_loader = utils.data.DataLoader(test_data, batch_size=batch_size, 
+            num_workers=num_workers)
+
+#specify image classes
+classes = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck']
+
+# obtain one batch of training images
+dataiter = iter(train_loader)
+images, labels = dataiter.next()
+images = images.numpy()
+
+# helper function to un-normalize and display an image
+def imshow(img):
+        img = img / 2 + 0.5  # unnormalize
+        plt.imshow(np.transpose(img, (1, 2, 0)))  # convert from Tensor image
+
+# plot the images in the batch, along with the corresponding labels
+fig = plt.figure(figsize=(25, 4))
+for idx in np.arange(20):
+    ax = fig.add_subplot(2, 20/2, idx+1, xticks=[], yticks=[])
+    imshow(images[idx])
+    # print out the correct label for each image
+    # .item() gets the value contained in a Tensor
+    ax.set_title(classes[labels[idx]])
+
+#define the NN arch
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        '''self.conv1 = nn.Conv2d(3, 6, 5)#, padding=1)
+        self.fc1 = nn.Linear(6 * 5*5,10,bias=True)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.dropout = nn.Dropout(0.2)'''
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        #x = self.pool(F.relu(self.conv2(x)))
+        #x = x.view(-1, 6* 5*5)
+        #x = self.fc1(x)
+        return x
+
+model = Net()
+print(model)
+
+#specify Cross Entropy loss function and SGD optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr = 0.001)
+
+epochs = 1
+#track min validation loss
+valid_loss_min = np.Inf
+
+for epoch in range(epochs):
+    train_loss = 0.0
+    valid_loss = 0.0
+
+    model.train()
+    for data, target in train_loader:
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output,target)
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item()*data.size(0)
+
+    #validate model
+    model.eval()
+    for data, target in valid_loader:
+        output = model(data)
+        loss = criterion(output, target)
+        valid_loss += loss.item()*data.size(0)
+
+    train_loss = train_loss/len(train_loader.dataset)
+    valid_loss = valid_loss/len(valid_loader.dataset)
+    print('Epoch {} \tTraining Loss: {:.6f} \tValid Loss: {:.6f}'.format(epoch+1, train_loss, valid_loss))
+
+    #save model if validation has decreased
+    if valid_loss <= valid_loss_min:
+        print('Validation loss decreased ({:.6f} --> {:.6f}).  Saving model ...'.format(
+        valid_loss_min, valid_loss))
+        torch.save(model.state_dict(), 'model.pth')
+        valid_loss_min = valid_loss
+
+#Test stage
+test_loss = 0.0
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+
+model.eval()
+
+for data, target in test_loader:
+    output = model(data)
+    loss = criterion(output, target)
+    test_loss += loss.item()*data.size(0)
+    _, pred = torch.max(output, 1)
+    correct = np.squeeze(pred.eq(target.data.view_as(pred)))
+
+    for i in range(batch_size):
+        label = target.data[i]
+        class_correct[label] += correct[i].item()
+        class_total[label] += 1
+
+# calculate and print avg test loss
+test_loss = test_loss/len(test_loader.dataset)
+print('Test Loss: {:.6f}\n'.format(test_loss))
+
+
+for i in range(10):
+    if class_total[i] > 0:
+        print('Test Accuracy of %5s: %2d%% (%2d/%2d)' % (
+            str(i), 100 * class_correct[i] / class_total[i],
+            np.sum(class_correct[i]), np.sum(class_total[i])))
+    else:
+        print('Test Accuracy of %5s: N/A (no training examples)' % (classes[i]))
+
+print('\nTest Accuracy (Overall): %2d%% (%2d/%2d)' % (
+    100. * np.sum(class_correct) / np.sum(class_total),
+    np.sum(class_correct), np.sum(class_total)))
+
+# obtain one batch of test images
+dataiter = iter(test_loader)
+images, labels = dataiter.next()
+
+# get sample outputs
+output = model(images)
+# convert output probabilities to predicted class
+_, preds = torch.max(output, 1)
+# prep images for display
+images = images.numpy()
+
+# plot the images in the batch, along with predicted and true labels
+fig = plt.figure(figsize=(25, 4))
+for idx in np.arange(20):
+    ax = fig.add_subplot(2, 20/2, idx+1, xticks=[], yticks=[])
+    imshow(images[idx])
+    ax.set_title("{} ({})".format(classes[preds[idx]], classes[labels[idx]]),
+                 color=("green" if preds[idx]==labels[idx] else "red"))
