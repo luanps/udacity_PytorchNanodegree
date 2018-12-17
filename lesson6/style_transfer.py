c685d718fa7282f8f4891a59a15408c4caa989c5
Luan <luann.porfirio@gmail.com>
2018-12-17 09:52:54 -0200

    train and test Style Transfer Network

--- lesson6/style_transfer.py
+++ lesson6/style_transfer.py
@@ -38,9 +38,9 @@ def load_image(img_path, max_size=400, shape=None):
     return image
 
 # load in content and style image
-content = load_image('images/octopus.jpg').to(device)
+content = load_image('/home/luan/Downloads/asa1.jpg')
 # Resize style to match content, makes code easier
-style = load_image('images/hockney.jpg', shape=content.shape[-2:]).to(device)
+style = load_image('/home/luan/Downloads/UIHere.png', shape=content.shape[-2:])
 
 # helper function for un-normalizing an image
 # and converting it from a Tensor image to a NumPy image for display
@@ -59,3 +59,98 @@ fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
 # content and style ims side-by-side
 ax1.imshow(im_convert(content))
 ax2.imshow(im_convert(style))
+
+print(vgg)
+
+def get_features(image, model, layers=None):
+    """ Run an image forward through a model and get the features for
+        a set of layers. Default layers are for VGGNet matching Gatys et al (2016)
+    """
+    if layers is None:
+        layers = {'0': 'conv1_1', '5': 'conv2_1',
+                '10': 'conv3_1', '19': 'conv4_1',
+                '21': 'conv4_2', '28': 'conv5_1'}
+
+    features = {}
+    x = image
+    # model._modules is a dictionary holding each module in the model
+    for name, layer in model._modules.items():
+        x = layer(x)
+        if name in layers:
+            features[layers[name]] = x
+
+    return features
+
+def gram_matrix(tensor):
+    ## get the batch_size, depth, height, and width of the Tensor
+    ## reshape it, so we're multiplying the features for each channel
+    ## calculate the gram matrix
+    _, d, h, w = tensor.size()
+    tensor = tensor.view(d,h*w)
+    gram = torch.mm(tensor,tensor.t())
+    return gram  
+
+# get content and style features only once before training
+content_features = get_features(content, vgg)
+style_features = get_features(style, vgg)
+
+# calculate the gram matrices for each layer of our style representation
+style_grams = {layer: gram_matrix(style_features[layer]) for layer in style_features}
+
+# create a third "target" image and prep it for change
+# it is a good idea to start of with the target as a copy of our *content* image
+# then iteratively change its style
+target = content.clone().requires_grad_(True)
+
+# weights for each style layer
+# weighting earlier layers more will result in *larger* style artifacts
+# notice we are excluding `conv4_2` our content representation
+style_weights = {'conv1_1': 1.,
+                 'conv2_1': 0.8,
+                 'conv3_1': 0.5,
+                 'conv4_1': 0.3,
+                 'conv5_1': 0.1}
+
+content_weight = 1  # alpha
+style_weight = 1e6  # beta
+
+# for displaying the target image, intermittently
+show_every = 400
+
+# iteration hyperparameters
+optimizer = optim.Adam([target], lr=0.003)
+    steps =500#2000  
+
+for ii in range(1, steps+1):
+
+    ## the content loss
+    target_features = get_features(target, vgg)
+    content_loss = torch.mean((target_features['conv4_2'] - content_features['conv4_2'])**2)
+
+    # the style loss
+    # initialize the style loss to 0
+    style_loss = 0
+    # iterate through each style layer and add to the style loss
+    for layer in style_weights:
+        # get the "target" style representation for the layer
+        target_feature = target_features[layer]
+        _, d, h, w = target_feature.shape
+        target_gram = gram_matrix(target_feature)
+        style_gram = style_grams[layer]
+
+        layer_style_loss = style_weights[layer] * torch.mean((target_gram - style_gram)**2)
+        # add to the style loss
+        style_loss += layer_style_loss / (d * h * w)
+
+    total_loss = content_weight * content_loss + style_weight * style_loss
+    # update your target image
+    optimizer.zero_grad()
+    total_loss.backward()
+    optimizer.step()
+
+    # display intermediate images and print the loss
+    if  ii % show_every == 0:
+        print('Total loss: ', total_loss.item())
+        plt.imshow(im_convert(target))
+        plt.show()
+    plt.imsave('fig.jpg',im_convert(target))
