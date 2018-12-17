5dff934d24f81d534447fae51be296f07d7d8649
Luan <luann.porfirio@gmail.com>
2018-12-17 08:39:36 -0200

    Lesson 6: Style Transfer with Deep Neural Networks

---
+++ lesson6/style_transfer.py
@@ -0,0 +1,61 @@
+import torch
+from torch import nn,optim
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+from torchvision import transforms, models
+
+# get the "features" portion of VGG19 (we will not need the "classifier" portion)
+vgg = models.vgg19(pretrained=True).features
+
+# freeze all VGG parameters since we're only optimizing the target image
+for param in vgg.parameters():
+        param.requires_grad_(False)
+
+def load_image(img_path, max_size=400, shape=None):
+    ''' Load in and transform an image, making sure the image
+           is <= 400 pixels in the x-y dims.'''
+    image = Image.open(img_path).convert('RGB')
+
+    # large images will slow down processing
+    if max(image.size) > max_size:
+        size = max_size
+    else:
+        size = max(image.size)
+    
+    if shape is not None:
+        size = shape
+
+    in_transform = transforms.Compose([
+                    transforms.Resize(size),
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.485, 0.456, 0.406),
+                                        (0.229, 0.224, 0.225))])
+
+    # discard the transparent, alpha channel (that's the :3) and add the batch dimension
+    image = in_transform(image)[:3,:,:].unsqueeze(0)
+
+    return image
+
+# load in content and style image
+content = load_image('images/octopus.jpg').to(device)
+# Resize style to match content, makes code easier
+style = load_image('images/hockney.jpg', shape=content.shape[-2:]).to(device)
+
+# helper function for un-normalizing an image
+# and converting it from a Tensor image to a NumPy image for display
+def im_convert(tensor):
+    """ Display a tensor as an image. """
+    image = tensor.to("cpu").clone().detach()
+    image = image.numpy().squeeze()
+    image = image.transpose(1,2,0)
+    image = image * np.array((0.229, 0.224, 0.225)) + np.array((0.485, 0.456, 0.406))
+    image = image.clip(0, 1)
+
+    return image
+
+# display the images
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
+# content and style ims side-by-side
+ax1.imshow(im_convert(content))
+ax2.imshow(im_convert(style))
