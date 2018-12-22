06526475bf9c4ed66ec84b2b5f1d933ee6331564
Luan <luann.porfirio@gmail.com>
2018-12-22 09:55:38 -0200

    Train RNN

--- lesson7/simple_rnn.py
+++ lesson7/simple_rnn.py
@@ -67,3 +67,68 @@ print('Input size: ', test_input.size())
 test_out, test_h = test_rnn(test_input, None)
 print('Output size: ', test_out.size())
 print('Hidden state size: ', test_h.size())
+
+# decide on hyperparameters
+input_size=1 
+output_size=1
+hidden_dim=64
+n_layers=1
+
+# instantiate an RNN
+rnn = RNN(input_size, output_size, hidden_dim, n_layers)
+print(rnn)
+
+# MSE loss and Adam optimizer with a learning rate of 0.01
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(rnn.parameters(), lr=0.01)
+
+# train the RNN
+def train(rnn, n_steps, print_every):
+
+    # initialize the hidden state
+    hidden = None
+
+    for batch_i, step in enumerate(range(n_steps)):
+        # defining the training data
+        time_steps = np.linspace(step * np.pi, (step+1)*np.pi, seq_length + 1)
+        data = np.sin(time_steps)
+        data.resize((seq_length + 1, 1)) # input_size=1
+
+        x = data[:-1]
+        y = data[1:]
+
+        # convert data into Tensors
+        x_tensor = torch.Tensor(x).unsqueeze(0) # unsqueeze gives a 1, batch_size dimension
+        y_tensor = torch.Tensor(y)
+
+        # outputs from the rnn
+        prediction, hidden = rnn(x_tensor, hidden)
+
+        ## Representing Memory ##
+        # make a new variable for hidden and detach the hidden state from its history
+        # this way, we don't backpropagate through the entire history
+        hidden = hidden.data
+
+        # calculate the loss
+        loss = criterion(prediction, y_tensor)
+        # zero gradients
+        optimizer.zero_grad()
+        # perform backprop and update weights
+        loss.backward()
+        optimizer.step()
+
+        # display loss and predictions
+        if batch_i%print_every == 0:
+            print('Loss: ', loss.item())
+            plt.plot(time_steps[1:], x, 'r.') # input
+            plt.plot(time_steps[1:], prediction.data.numpy().flatten(), 'b.') # predictions
+            plt.show()
+
+    return rnn
+
+
+# train the rnn and monitor results
+n_steps = 75
+print_every = 15
+
+trained_rnn = train(rnn, n_steps, print_every)
