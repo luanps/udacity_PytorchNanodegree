716e111d12d308c569e1efc33c4eb202a8812468
Luan <luann.porfirio@gmail.com>
2018-12-22 21:09:16 -0200

    add CharRNN class

--- lesson7/char-level-rnn.py
+++ lesson7/char-level-rnn.py
@@ -43,15 +43,15 @@ def get_batches(arr, batch_size, seq_length):
        batch_size: Batch size, the number of sequences per batch
        seq_length: Number of encoded chars in a sequence
     '''
-    ## TODO: Get the number of batches we can make
+    # Get the number of batches we can make
     n_batches = int(len(arr)/(batch_size*seq_length))
-    ## TODO: Keep only enough characters to make full batches
+    # Keep only enough characters to make full batches
     arr =  arr[:n_batches*batch_size*seq_length]
     
-    ## TODO: Reshape into batch_size rows
+    # Reshape into batch_size rows
     arr = arr.reshape(batch_size,-1)
 
-    ## TODO: Iterate over the batches using a window of size seq_length
+    # Iterate over the batches using a window of size seq_length
     for n in range(0, arr.shape[1], seq_length):
         # The features
         x = arr[:,n:n+seq_length]
@@ -62,4 +62,64 @@ def get_batches(arr, batch_size, seq_length):
         except IndexError:
             y[:, :-1], y[:, -1] = x[:, 1:], arr[:, 0]
         yield x, y
+
+# check if GPU is available
+train_on_gpu = torch.cuda.is_available()
+if(train_on_gpu):
+    print('Training on GPU!')
+else:
+    print('No GPU available, training on CPU; consider making n_epochs very small.')
+
+class CharRNN(nn.Module):
+
+    def __init__(self, tokens, n_hidden=256, n_layers=2,
+                               drop_prob=0.5, lr=0.001):
+        super().__init__()
+        self.drop_prob = drop_prob
+        self.n_layers = n_layers
+        self.n_hidden = n_hidden
+        self.lr = lr
+
+        # creating character dictionaries
+        self.chars = tokens
+        self.int2char = dict(enumerate(self.chars))
+        self.char2int = {ch: ii for ii, ch in self.int2char.items()}
+
+        self.lstm = nn.LSTM(len(self.chars), n_hidden, n_layers, 
+                    dropout = drop_prob, batch_first=True)
+        self.dropout = nn.Dropout(drop_prob)
+        self.fc = nn.Linear(n_hidden, len(self.chars))
+
+    def forward(self, x, hidden):
+        ''' Forward pass through the network.
+            These inputs are x, and the hidden/cell state `hidden`. '''
+
+        # Get the outputs and the new hidden state from the lstm
+        r_output, hidden = self.lstm(x, hidden)
+        out = self.dropout(r_output)
+
+        # Stack up LSTM outputs using view
+        # use contiguous to reshape the output
+        out = out.contiguous().view(-1, self.n_hidden)
+
+        # put x through the fully-connected layer
+        out = self.fc(out)
+        # return the final output and the hidden state
+        return out, hidden
+
+
+    def init_hidden(self, batch_size):
+        ''' Initializes hidden state '''
+        # Create two new tensors with sizes n_layers x batch_size x n_hidden,
+        # initialized to zero, for hidden state and cell state of LSTM
+        weight = next(self.parameters()).data
+
+        if (train_on_gpu):
+            hidden = (weight.new(self.n_layers, batch_size, self.n_hidden).zero_().cuda(),
+                    weight.new(self.n_layers, batch_size, self.n_hidden).zero_().cuda())
+        else:
+            hidden = (weight.new(self.n_layers, batch_size, self.n_hidden).zero_(),
+                    weight.new(self.n_layers, batch_size, self.n_hidden).zero_())
+
+        return hidden
 
