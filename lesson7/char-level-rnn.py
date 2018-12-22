dab521ccad701a13f6dedaea83916e81480c8597
Luan <luann.porfirio@gmail.com>
2018-12-22 20:41:35 -0200

    get LSTM batches from data

--- lesson7/char-level-rnn.py
+++ lesson7/char-level-rnn.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 from torch import nn
 import torch.nn.functional as F
-
+import pdb
 with open('anna.txt', 'r') as f:
     text = f.read()
 
@@ -29,7 +29,37 @@ def one_hot_encode(arr, n_labels):
     return one_hot
 
 # check that the function works as expected
-test_seq = np.array([[3, 5, 1]])
+test_seq = np.array([[3, 5, 1, 2, 7, 0]])
 one_hot = one_hot_encode(test_seq, 8)
-
 print(one_hot)
+
+def get_batches(arr, batch_size, seq_length):
+    '''Create a generator that returns batches of size
+       batch_size x seq_length from arr.
+       
+       Arguments
+       ---------
+       arr: Array you want to make batches from
+       batch_size: Batch size, the number of sequences per batch
+       seq_length: Number of encoded chars in a sequence
+    '''
+    ## TODO: Get the number of batches we can make
+    n_batches = int(len(arr)/(batch_size*seq_length))
+    ## TODO: Keep only enough characters to make full batches
+    arr =  arr[:n_batches*batch_size*seq_length]
+    
+    ## TODO: Reshape into batch_size rows
+    arr = arr.reshape(batch_size,-1)
+
+    ## TODO: Iterate over the batches using a window of size seq_length
+    for n in range(0, arr.shape[1], seq_length):
+        # The features
+        x = arr[:,n:n+seq_length]
+        # The targets, shifted by one
+        y = np.zeros_like(x)
+        try:
+            y[:, :-1], y[:, -1] = x[:, 1:], arr[:, n+seq_length]
+        except IndexError:
+            y[:, :-1], y[:, -1] = x[:, 1:], arr[:, 0]
+        yield x, y
+
