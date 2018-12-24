ac874a92181296edf3f1d0d64b50e99fb9325da1
Luan <luann.porfirio@gmail.com>
2018-12-24 13:50:58 -0200

    pad features to a fixed length

--- lesson8/sentiment_rnn.py
+++ lesson8/sentiment_rnn.py
@@ -41,3 +41,28 @@ for ii, review in enumerate(reviews_ints):
         reviews_ints.pop(ii)
         encoded_labels = np.delete(encoded_labels,ii)
 
+def pad_features(reviews_ints, seq_length):
+    ''' Return features of review_ints, where each review is padded with 0's 
+        or truncated to the input seq_length.
+    '''
+    features = reviews_ints.copy()
+    for ii,review in enumerate(reviews_ints):
+        length = len(review)
+        while length < seq_length:
+            features[ii].insert(0,0)
+            length += 1
+        if length >= seq_length:
+            features[ii] = features[ii][:seq_length]
+    
+    return np.asarray(features)
+
+# Test your implementation!
+
+seq_length = 200
+
+features = pad_features(reviews_ints, seq_length=seq_length)
+## test statements - do not change - ##
+assert len(features)==len(reviews_ints), "Your features should have as many rows as reviews."
+assert len(features[0])==seq_length, "Each feature row should contain seq_length values."
+# print first 10 values of the first 30 batches
+print(features[:30,:10])
